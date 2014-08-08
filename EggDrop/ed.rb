a4ed63990cda8a8b6dafd3dc91b611ab0482e52b
cema-sp <cema.rus@gmail.com>
2014-08-08 11:10:37 +0400

    Egg Drop Algorithm

--- EggDrop/ed.rb
+++ EggDrop/ed.rb
@@ -20,45 +20,99 @@ class EggsDrop
     puts "Given: #@height -floor building with level = #@level"
   end
   def print_result(alg, res)
-    puts "\n#{alg} done with #@eggs eggs and #@tosses tosses. Level = #{res}"
+    puts "\n#{alg} done with #@eggs eggs and #@tosses tosses. Found level = #{res}"
   end
   def alg_1
-    # algorithm with l OofG
+    # OofG(eggs) = 1
+    # OofG(tosses) = L
     reset_stats
 
-    h = 0
+    res = 0
     found = false
     while !found
-      h += 1
-      if !toss(h)
+      res += 1
+      if !toss(res)
         found = true
       end
     end
 
-    print_result("Alg_1", h)
+    print_result("Alg_1", res)
   end
   def alg_2
-    # algorithm with l OofG
+    # OofG(eggs) = log_2(H)
+    # OofG(tosses) = log_2(H)
     reset_stats
 
     bottom = 1
     top = @height
-    while top>=bottom
-      h = (bottom+top)/2
-      if toss(h)
-        bottom = h+1
+    while top>bottom
+      res = (bottom+top)/2
+      if toss(res)
+        bottom = res+1
       else
-        top = h-1
+        top = res
+      end
+    end
+    res = (bottom+top)/2
+
+    print_result("Alg_2", res)
+  end
+  def alg_3
+    # OofG(eggs) = 2
+    # OofG(tosses) = 2sqrt(H)
+    reset_stats
+
+    step = (@height**(0.5)).to_i
+    current = step
+
+    while current<=@height
+      break if !toss(current)
+      current += step
+    end
+
+    found = false
+    res = current - step
+    while !found
+      res += 1
+      if !toss(res)
+        found = true
       end
     end
 
-    print_result("Alg_2", h)
+    print_result("Alg_3", res)
+  end
+  def alg_4
+    # OofG(eggs) = 2
+    # OofG(tosses) = 
+    reset_stats
+
+    step = (((8*@height+1)**(0.5)-1)/2).ceil
+    current = step
+
+    while current<@height
+      break if !toss(current)
+      step -= 1
+      current += step
+    end
+
+    found = false
+    res = current - step
+    while !found
+      res += 1
+      if !toss(res)
+        found = true
+      end
+    end
+
+    print_result("Alg_4", res)
   end
 end
 
 #---------------------------------------------------------
 
-ed = EggsDrop.new(100,45)
+ed = EggsDrop.new(100,99)
 ed.print_inputs
 ed.alg_1
 ed.alg_2
+ed.alg_3
+ed.alg_4
