6bee979b2bdb63d8ef56137f38c654e5c5f4899d
cema-sp <cema.rus@gmail.com>
2014-08-08 11:10:12 +0400

    Bitonic Array Search

--- BitonicArraySearch/ba.rb
+++ BitonicArraySearch/ba.rb
@@ -20,55 +20,108 @@ class BitonicArray
       @array[i] = prev
     end
   end
-  def search(x)
-    # 2-loop binary search (fail)
-    bottom_1 = 0
-    top_1 = @array.size-1
-    mid_1 = (top_1 + bottom_1) / 2
-    while top_1 >= bottom_1
-      id = (top_1 + bottom_1) / 2
-      if @array[id] == x
-        return id
-      elsif x > @array[id]
-        bottom_1 = id + 1
-      else
-        top_1 = id - 1
-      end
-    end
+  def search2(x)
+    # OofG = 3log(N base 2)
+    top = @array.size - 1
+    bottom = 0
 
-    bottom_2 = mid_1 + 1
-    top_2 = @array.size-1
-    while top_2 >= bottom_2
-      id = (top_2 + bottom_2) / 2
-      if @array[id] == x
-        return id
-      elsif x > @array[id]
-        top_1 = id - 1
+    while top>bottom
+      mid = (bottom+top)/2
+      value_mid = @array[mid]
+      value_mid_next = @array[mid+1]
+      if x==value_mid
+        return mid
+      elsif x>value_mid
+        if value_mid>value_mid_next
+          top = mid-1
+        else
+          bottom = mid+1
+        end
       else
-        bottom_1 = id + 1
+        bottom_1 = bottom
+        top_1 = mid-1
+
+        while top_1>=bottom_1
+          mid_1 = (top_1+bottom_1)/2
+          val_1 = @array[mid_1]
+          if x==val_1
+            return mid_1
+          elsif x<val_1
+            top_1 = mid_1-1
+          else
+            bottom_1 = mid_1+1
+          end
+        end
+
+        bottom_2 = mid+1
+        top_2 = top
+
+        while top_2>=bottom_2
+          mid_2 = (top_2+bottom_2)/2
+          val_2 = @array[mid_2]
+          if x==val_2
+            return mid_2
+          elsif x>val_2
+            top_2 = mid_2-1
+          else
+            bottom_2 = mid_2+1
+          end
+        end
       end
     end
   end
-  def middle
+  def search3(x)
+    # 2-loop binary search
+    # OofG = 2log(N base 2)
+    max = self.index_max
+
+    top = max
     bottom = 0
-    top = @array.size-1
-    while top>bottom
-      first = (2*bottom+top)/3
-      second = (bottom+2*top)/3
-      #puts "bottom=#{bottom}, first=#{first}, second=#{second}, top=#{top}"
-      if first==second
-        if @array[top]>@array[bottom]
-          return top
-        else
-          return bottom
-        end
-      elsif @array[first]<@array[second]
-        bottom = first+1
+
+    while top>=bottom
+      mid = (top+bottom)/2
+      val = @array[mid]
+      if x>val
+        bottom = mid+1
+      elsif x<val
+        top = mid-1
       else
-        top = second-1
+        return mid
       end
-      #puts "bottom=#{bottom}, first=#{first}, second=#{second}, top=#{top}"
     end
+
+    top = @array.size-1
+    bottom = max+1
+
+    while top>=bottom
+      mid = (top+bottom)/2
+      val = @array[mid]
+      if x<val
+        bottom = mid+1
+      elsif x>val
+        top = mid-1
+      else
+        return mid
+      end
+    end
+
+    return nil
+  end
+  def index_max
+    # index of middle element
+    # OofG = log(N)
+    top = @array.size - 1
+    bottom = 0
+
+    while top>bottom
+      mid = (bottom+top)/2
+      if @array[mid]>=@array[mid+1]
+        top = mid
+      else
+        bottom = mid+1
+      end
+    end
+    top
   end
 end
 
@@ -84,10 +137,8 @@ while inp!='q'
   command, id = inp.split(' ')
   case command
   when 's'
-    then p ba.search(id.to_i)
+    then p ba.search2(id.to_i)
   when 'show'
     then p ba.array
-  when 'mid'
-    then p ba.middle
   end
 end
